27556cbc6d6528a8eedeafbb8fc71f00b87d9978
KevinKhuu27 <khuu.kevin27@hotmail.com>
2026-09-05 17:21:57 -0400

    Added about me section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
 import { Landing } from './sections/landing/landing';
+import { About } from './sections/about/about';
 /*
-import { AboutComponent } from './sections/about';
 import { ProjectsComponent } from './sections/projects';
 */
 import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Landing, Footer],
+  imports: [RouterOutlet, Navbar, Landing, About, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
