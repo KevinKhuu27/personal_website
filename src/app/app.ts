dc5f389148c93b116cf6a8d6120a8d0e38f5b656
KevinKhuu27 <khuu.kevin27@hotmail.com>
2026-09-06 17:28:23 -0400

    Updated warnings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
 import { Landing } from './sections/landing/landing';
 import { About } from './sections/about/about';
@@ -8,7 +7,7 @@ import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Landing, About, Projects, Footer],
+  imports: [Navbar, Landing, About, Projects, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
